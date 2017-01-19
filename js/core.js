358ee8f7ecccd854115756b2fa82c5e9f037c0da
Parallel <mao@malatoday.com>
2017-01-19 15:50:23 +0800

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,1 +1,21 @@
+var num_of_dice = localStorage.num_of_dice == undefined ? 1 : parseInt(localStorage.num_of_dice);
+var max_size_of_dice = localStorage.max_size_of_dice == undefined ? 12 : parseInt(localStorage.max_size_of_dice);
+
+function rollDice(size) {
+    return Math.ceil(Math.random()*size);
+}
+
+function rollManyDices(number, size) {
+    if (number <= 0) {
+        return false;
+    }
+  
+    var result = [];
+  
+    while(number-- > 0) {
+        result.push(roleDice(size));
+    }
+  
+    return result;
+}
 
